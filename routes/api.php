8e557e42755e7a75496efb6899dd2f2bf1032872
putrilestariw <nindysanjaya28@gmail.com>
2021-11-16 22:31:41 +0700

    tugas pi 7

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+
+use App\Http\Controllers\Api\CobaController;
+use App\Http\Controllers\Api\GroupsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('', [CobaController::class, 'index']);
+Route::resources([
+    'friends' => CobaController::class,
+    'groups'=> GroupsController::class,
+]);
